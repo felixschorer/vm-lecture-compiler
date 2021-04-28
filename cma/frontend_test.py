6df00ae42375d4479ee4386380d4cb2d50141de4
Felix Schorer <felixschorer@users.noreply.github.com>
2021-04-28 18:20:51 +0200

    Cleanup tests

--- cma/frontend_test.py
+++ cma/frontend_test.py
@@ -19,8 +19,8 @@ from cma.frontend import (
 class TestParserConstant(unittest.TestCase):
     def test_parsing_constant(self):
         data = "42"
-        result = C.Expression.parseString(data, parseAll=True).asList()
-        desired = [Constant(value=42)]
+        (result,) = C.Expression.parseString(data, parseAll=True)
+        desired = Constant(value=42)
         self.assertEqual(result, desired)
 
     def test_parsing_constant_truncation(self):
@@ -32,8 +32,8 @@ class TestParserConstant(unittest.TestCase):
 class TestParserIdentifier(unittest.TestCase):
     def test_parsing_identifier(self):
         data = "asdf"
-        result = C.Expression.parseString(data, parseAll=True).asList()
-        desired = [Identifier(name="asdf")]
+        (result,) = C.Expression.parseString(data, parseAll=True)
+        desired = Identifier(name="asdf")
         self.assertEqual(result, desired)
 
     def test_parsing_identifier_truncation(self):
@@ -45,46 +45,42 @@ class TestParserIdentifier(unittest.TestCase):
 class TestParserBinOp(unittest.TestCase):
     def test_parsing_binop_minus_constants(self):
         data = "42 - 1"
-        result = C.Expression.parseString(data, parseAll=True).asList()
-        desired = [BinaryOp(left=Constant(value=42), op="-", right=Constant(value=1))]
+        (result,) = C.Expression.parseString(data, parseAll=True)
+        desired = BinaryOp(left=Constant(value=42), op="-", right=Constant(value=1))
         self.assertEqual(result, desired)
 
     def test_parsing_binop_plus_identifier(self):
         data = "a + b"
-        result = C.Expression.parseString(data, parseAll=True).asList()
-        desired = [
-            BinaryOp(left=Identifier(name="a"), op="+", right=Identifier(name="b"))
-        ]
+        (result,) = C.Expression.parseString(data, parseAll=True).asList()
+        desired = BinaryOp(
+            left=Identifier(name="a"), op="+", right=Identifier(name="b")
+        )
         self.assertEqual(result, desired)
 
     def test_parsing_binop_precedence(self):
         data = "3 + d / 5"
-        result = C.Expression.parseString(data, parseAll=True).asList()
-        desired = [
-            BinaryOp(
-                left=Constant(value=3),
-                op="+",
-                right=BinaryOp(
-                    left=Identifier(name="d"), op="/", right=Constant(value=5)
-                ),
-            )
-        ]
+        (result,) = C.Expression.parseString(data, parseAll=True)
+        desired = BinaryOp(
+            left=Constant(value=3),
+            op="+",
+            right=BinaryOp(left=Identifier(name="d"), op="/", right=Constant(value=5)),
+        )
         self.assertEqual(result, desired)
 
 
 class TestParserUnaryOp(unittest.TestCase):
     def test_parsing_unaryop_minus_constants(self):
         data = "-1"
-        result = C.Expression.parseString(data, parseAll=True).asList()
-        desired = [UnaryOp(op="-", expr=Constant(value=1))]
+        (result,) = C.Expression.parseString(data, parseAll=True)
+        desired = UnaryOp(op="-", expr=Constant(value=1))
         self.assertEqual(result, desired)
 
 
 class TestParserAssignment(unittest.TestCase):
     def test_parsing_correct_constant_assignment(self):
         data = "x = 3"
-        result = C.Expression.parseString(data, parseAll=True).asList()
-        desired = [Assignment(left=Identifier(name="x"), right=Constant(value=3))]
+        (result,) = C.Expression.parseString(data, parseAll=True)
+        desired = Assignment(left=Identifier(name="x"), right=Constant(value=3))
         self.assertEqual(result, desired)
 
     def test_parsing_incorrect_constant_assignment(self):
@@ -94,15 +90,11 @@ class TestParserAssignment(unittest.TestCase):
 
     def test_parsing_correct_binop_assignment(self):
         data = "x = 3 + y"
-        result = C.Expression.parseString(data, parseAll=True).asList()
-        desired = [
-            Assignment(
-                left=Identifier(name="x"),
-                right=BinaryOp(
-                    left=Constant(value=3), op="+", right=Identifier(name="y")
-                ),
-            )
-        ]
+        (result,) = C.Expression.parseString(data, parseAll=True)
+        desired = Assignment(
+            left=Identifier(name="x"),
+            right=BinaryOp(left=Constant(value=3), op="+", right=Identifier(name="y")),
+        )
         self.assertEqual(result, desired)
 
     def test_parsing_incorrect_binop_assignment(self):
@@ -114,55 +106,49 @@ class TestParserAssignment(unittest.TestCase):
 class TestParserFuncCall(unittest.TestCase):
     def test_parsing_func_call(self):
         data = "fib(n - 1) + fib(n - 2)"
-        result = C.Expression.parseString(data, parseAll=True).asList()
-        desired = [
-            BinaryOp(
-                left=FuncCall(
-                    identifier=Identifier(name="fib"),
-                    arguments=FuncCallArguments(
-                        BinaryOp(
-                            left=Identifier(name="n"), op="-", right=Constant(value=1)
-                        ),
+        (result,) = C.Expression.parseString(data, parseAll=True)
+        desired = BinaryOp(
+            left=FuncCall(
+                identifier=Identifier(name="fib"),
+                arguments=FuncCallArguments(
+                    BinaryOp(
+                        left=Identifier(name="n"), op="-", right=Constant(value=1)
                     ),
                 ),
-                op="+",
-                right=FuncCall(
-                    identifier=Identifier(name="fib"),
-                    arguments=FuncCallArguments(
-                        BinaryOp(
-                            left=Identifier(name="n"), op="-", right=Constant(value=2)
-                        ),
+            ),
+            op="+",
+            right=FuncCall(
+                identifier=Identifier(name="fib"),
+                arguments=FuncCallArguments(
+                    BinaryOp(
+                        left=Identifier(name="n"), op="-", right=Constant(value=2)
                     ),
                 ),
-            )
-        ]
+            ),
+        )
         self.assertEqual(result, desired)
 
 
 class TestParserStatement(unittest.TestCase):
     def test_parse_plain_statement(self):
         data = "x = 42;"
-        result = C.Statement.parseString(data, parseAll=True).asList()
-        desired = [
-            PlainStatement(
-                expr=Assignment(left=Identifier(name="x"), right=Constant(value=42))
-            )
-        ]
+        (result,) = C.Statement.parseString(data, parseAll=True)
+        desired = PlainStatement(
+            expr=Assignment(left=Identifier(name="x"), right=Constant(value=42))
+        )
         self.assertEqual(result, desired)
 
     def test_parse_statements(self):
         data = "x = 42; y = 2;"
-        result = C.StatementSequence.parseString(data, parseAll=True).asList()
-        desired = [
-            StatementSequence(
-                PlainStatement(
-                    expr=Assignment(left=Identifier(name="x"), right=Constant(value=42))
-                ),
-                PlainStatement(
-                    expr=Assignment(left=Identifier(name="y"), right=Constant(value=2))
-                ),
-            )
-        ]
+        (result,) = C.StatementSequence.parseString(data, parseAll=True)
+        desired = StatementSequence(
+            PlainStatement(
+                expr=Assignment(left=Identifier(name="x"), right=Constant(value=42))
+            ),
+            PlainStatement(
+                expr=Assignment(left=Identifier(name="y"), right=Constant(value=2))
+            ),
+        )
         self.assertEqual(result, desired)
 
     def test_parse_incorrect_statements(self):
